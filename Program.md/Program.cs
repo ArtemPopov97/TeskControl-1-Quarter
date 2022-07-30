490160bbf880868d7d12cac7d0823605e2512b63
unknown <artem.popov.97@mail.ru>
2022-07-30 10:10:01 +0300

    Создания программы по КР

--- Program.md/Program.cs
+++ Program.md/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] Array1 = new string [4] {"1234", "1567", "-2", "computer science"};
+string [] Array2 = new string [Array1.Length];
+int count = 0;
+
+void CheckingArray (string [] Array1, string [] Array2)
+{
+    for (int i = 0; i < Array1.Length; i++)
+    {
+        if (Array1[i].Length <= 3)
+        {
+            Array2[count] = Array1[i];
+            count++;
+        }
+    }
+}
+
+void PrintMassiv (string [] Array2)
+{
+    for (int i = 0; i < Array2.Length; i++)
+    {
+        Console.WriteLine($"{Array2[i]} ");
+    }
+}
+
+CheckingArray(Array1,Array2);
+PrintMassiv(Array2);
